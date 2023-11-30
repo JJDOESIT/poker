382e127684ffca2526d9a7ba3469d845e2b2ed77
James <jamesgwhit@gmail.com>
2023-11-29 21:37:08 -0500

    port

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import _thread
 from data import Data
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = ('', 5555)
+address = ('', 6276)
 s.bind(address)
 s.listen(4)
 print("Listening...")
